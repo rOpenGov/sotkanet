c5fc7b7dd2e75c7f9133e8f4f5b6415400cdcfda
antagomir <antagomir@opencomp>
2014-02-25 16:05:28 +0100

    removed non-ascii from vignette

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,4 +1,4 @@
 ~/bin/R-3.0.1/bin/R CMD BATCH document.R
 ~/bin/R-3.0.1/bin/R CMD build ../../
-~/bin/R-3.0.1/bin/R CMD check --as-cran sotkanet_0.9.01.tar.gz
-~/bin/R-3.0.1/bin/R CMD INSTALL sotkanet_0.9.01.tar.gz
+~/bin/R-3.0.1/bin/R CMD check --as-cran sotkanet_0.9.02.tar.gz
+~/bin/R-3.0.1/bin/R CMD INSTALL sotkanet_0.9.02.tar.gz
