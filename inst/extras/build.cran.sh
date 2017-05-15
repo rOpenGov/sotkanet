4ff9cccde8b66eaa40f1dcabb14586a8bd2c3e6f
antagomir <leo.lahti@iki.fi>
2017-05-15 18:17:24 +0200

    all ok

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,4 +1,4 @@
 # /home/lei/bin/R CMD BATCH document.R
 /home/lei/bin/R CMD build ../../ --no-build-vignettes
-/home/lei/bin/R CMD check --as-cran sotkanet_0.9.36.tar.gz
-/home/lei/bin/R CMD INSTALL sotkanet_0.9.36.tar.gz
+/home/lei/bin/R CMD check --as-cran sotkanet_0.9.37.tar.gz
+/home/lei/bin/R CMD INSTALL sotkanet_0.9.37.tar.gz
