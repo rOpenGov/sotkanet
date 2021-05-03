8b0cef25f39ed880f1bd3892c36b78ed4a832bd0
Pyry Kantanen <pyry.kantanen@utu.fi>
2021-05-03 15:54:44 +0300

    Removed problematic URL from tutorial, small update to tests

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -1,6 +1,8 @@
+library(testthat)
+
 context("Get")
-test_that("get sotkanet includes year field",{
-  skip_on_cran()
-  expect_true("year" %in% names(GetDataSotkanet(indicators = 165, verbose = FALSE)))
+testthat::test_that("get sotkanet includes year field",{
+  testthat::skip_on_cran()
+  testthat::expect_true("year" %in% names(GetDataSotkanet(indicators = 165, verbose = TRUE)))
 })
 
