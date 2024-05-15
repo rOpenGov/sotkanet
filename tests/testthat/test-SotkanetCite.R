0d524f5be7003feba974dc225b01f817b37a8155
Allaht2 <allaht@utu.fi>
2024-05-15 16:44:36 +0300

    Add test for SotkanetCite

--- tests/testthat/test-SotkanetCite.R
+++ tests/testthat/test-SotkanetCite.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("SotkanetCite gives correct results", {
+
+  expect_equal(class(SotkanetCite(10013)), "Bibtex")
+
+  expect_equal(class(SotkanetCite(10012)), "Bibtex")
+
+  expect_equal(class(SotkanetCite(10011)), "Bibtex")
 })
