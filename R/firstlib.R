cea6ccc9eed21ae2743288968c2f916b065826c9
Pyry Kantanen <pyry.kantanen@gmail.com>
2024-05-15 14:55:20 +0300

    Update copyright years

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,4 +1,4 @@
 .onAttach <- function(lib, pkg)
 {
-  packageStartupMessage("Sotkanet R tools (C) 2013-2022 rOpenGov\nhttp://ropengov.org")
+  packageStartupMessage("Sotkanet R tools (C) 2013-2024 rOpenGov\nhttp://ropengov.org")
 }
