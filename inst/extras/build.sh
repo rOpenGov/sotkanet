ce00538e0e325ad77ae2dd8154bd164bc235de9c
antagomir <leo.lahti@iki.fi>
2024-07-10 11:01:13 +0300

    pkg checks ok

--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,4 +1,4 @@
 #/usr/local/bin/R CMD BATCH document.R
-~/bin/R-4.1.2/bin/R CMD build ../../ 
-~/bin/R-4.1.2/bin/R CMD check --as-cran sotkanet_0.9.79.tar.gz
-~/bin/R-4.1.2/bin/R CMD INSTALL sotkanet_0.9.79.tar.gz
+~/bin/R-4.3.2/bin/R CMD build ../../ 
+~/bin/R-4.3.2/bin/R CMD check --as-cran sotkanet_0.10.1.tar.gz
+~/bin/R-4.3.2/bin/R CMD INSTALL sotkanet_0.10.1.tar.gz
