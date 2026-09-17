f28d2aeae072da3dd090baf49dea30109b79c8fa
Pyry Kantanen <pyry.kantanen@gmail.com>
2026-09-17 17:36:05 +0300

    Rename functions in tests, remove httpstat.us due to it being down

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -2,24 +2,21 @@ library(testthat)
 
 testthat::test_that("get sotkanet includes year field",{
   testthat::skip_on_cran()
-  testthat::expect_true("year" %in% names(GetDataSotkanet(indicators = 165)))
+  testthat::expect_true("year" %in% names(get_sotkanet(indicators = 165)))
 })
 
 testthat::test_that("Invalid URLs produce a message",{
   testthat::skip_on_cran()
-  testthat::expect_error(sotkanet:::sotkanet.json_query("httpstat.us/200?sleep=1500", timeout = 1))
-  testthat::expect_message(test_connection("https://httpstat.us/401"))
-  testthat::expect_message(test_connection("https://httpstat.us/418"))
-  testthat::expect_message(SotkanetIndicatorMetadata())
+  testthat::expect_message(sotkanet_indicator_metadata())
   testthat::expect_message(SotkanetIndicators(type = "teibeul"))
-  testthat::expect_message(GetDataSotkanet(5468, years = 2020, genders = "total", regions = "Soumi"))
-  testthat::expect_message(GetDataSotkanet(5468, years = 2020, genders = "total", region.category = "KUU"))
+  testthat::expect_message(get_sotkanet(5468, years = 2020, genders = "total", regions = "Soumi"))
+  testthat::expect_message(get_sotkanet(5468, years = 2020, genders = "total", region.category = "KUU"))
 })
 
 testthat::test_that("Valid URLs stay silent or produce expected output",{
   testthat::skip_on_cran()
   testthat::expect_silent(sotkanet::sotkanet.json_query("http://httpbin.org/json"))
-  testthat::expect_s3_class(GetDataSotkanet(5468, years = 2020, genders = "total"), "data.frame")
-  testthat::expect_s3_class(SotkanetIndicatorMetadata(5468, type = "table"), "data.frame")
-  testthat::expect_s3_class(GetDataSotkanet(5468, years = 2020, genders = "total", regions = "Koko maa"), "data.frame")
+  testthat::expect_s3_class(get_sotkanet(5468, years = 2020, genders = "total"), "data.frame")
+  testthat::expect_s3_class(sotkanet_indicator_metadata(5468, type = "table"), "data.frame")
+  testthat::expect_s3_class(get_sotkanet(5468, years = 2020, genders = "total", regions = "Koko maa"), "data.frame")
 })
